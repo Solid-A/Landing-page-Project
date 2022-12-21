8397a5be742ff282bf01e610a33c10bdff15c06f
Ahmed Farouk <118807953+Solid-A@users.noreply.github.com>
2022-12-21 21:27:12 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,45 @@
+# Landing Page Project
+
+## Table of Contents
+
+* [Instructions](#instructions)
+
+## Instructions
+
+The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+
+To get started, open `js/app.js` and start building out the app's functionality
+
+For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+
+
+# Project Title
+
+-Multi-section landing page
+
+## Development Strategy
+
+1.Linking my ***app.js***.
+
+2.Building ***HTML*** and 4 content sections.
+
+3.Building the ***navigation*** menu.
+
+4.Dynamically create a navigation menu based on the ***sections*** of the page.
+
+5.Distinguish the ***section*** in view.
+
+6.scrolling to ***sections***.
+
+7.Add an ***active*** state to the navigation items when a section is in the viewport.
+
+8.Makeing sections ***collapsible***.
+
+9.Responsive landing page.
+
+10.Hide fixed navigation bar while not scrolling.
+
+11.Add a scroll to the top button
+
+## Test the performance
+
---
+++ js/app.js
@@ -0,0 +1,133 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+/**
+ * Comments should be present at the beginning of each procedure and class.
+ * Great to have comments before crucial code sections within the procedure.
+*/
+const t0 = performance.now();  //performance
+/**
+ * Define Global Variables
+ * 
+*/
+const selectUlNav = document.getElementById('navbar__list');
+const selectSectoin = document.querySelectorAll('section');
+const myDocFrag = document.createDocumentFragment();    // Document Fragment
+const collapsible = document.querySelectorAll("button");
+const head = document.querySelector('.page__header');
+const toTop = document.querySelector('#to-top');
+/**
+ * End Global Variables
+ *  
+ * 
+*/
+/**
+ *  
+ * Begin Main Functions
+ * 
+*/
+// build the nav
+const addSectoin = () => {
+    for(let item of selectSectoin){
+        let sectionId = item.getAttribute('id');
+        let sectionName = item.getAttribute('data-nav');
+        let addLi = document.createElement('li');
+        let addLink = document.createElement('a');
+        addLink.textContent = sectionName;
+        addLink.setAttribute('class', 'menu__link');
+        addLink.setAttribute('href', `#${sectionId}`);   // Scroll to section on link click
+        addLink.addEventListener('click', e =>{
+            e.preventDefault();
+            item.scrollIntoView({      // Scroll to anchor ID using scrollTO event
+                behavior: 'smooth'
+            });
+        })
+        addLi.appendChild(addLink);
+        myDocFrag.appendChild(addLi);
+    }
+    
+    selectUlNav.appendChild(myDocFrag);
+};
+addSectoin();
+
+// Add class 'active' to section when near top of viewport
+function makeActive(){
+    selectSectoin.forEach(active =>{
+        const activeView = active.getBoundingClientRect();
+        let selectNames = active.getAttribute('data-nav');
+        if(activeView.top <= 150 && activeView.bottom >= 300){
+            active.classList.add('active-class');
+            const selectLinks = document.querySelectorAll('a');
+            selectLinks.forEach(classLink =>{    // Set sections as active
+                if(classLink.textContent === selectNames){
+                    classLink.classList.add('active-link');
+                }else{
+                    classLink.classList.remove('active-link');
+                }
+            })
+        }else{
+            active.classList.remove('active-class');
+        }
+    })
+}
+document.addEventListener("scroll", function() { makeActive();});
+
+
+
+/**
+ * End Main Functions
+ * Begin Events
+ * 
+*/
+function collView(){
+    for (let i of collapsible) {
+        i.addEventListener("click", () => {
+            let content = i.nextElementSibling;
+            if (content.style.display === "block") {
+                    content.style.display = "none";
+            } else {
+                    content.style.display = "block";
+            }
+        })
+    }
+}
+collView();
+
+
+//Hide fixed navigation
+
+window.onscroll = () => {
+    head.style.transform = 'none';
+    setTimeout(() =>{
+        head.style.transform = 'Translate(0px, -60px)';
+        head.style.transition = 'all 0.5s ease-in-out';
+    }, 3000);
+};
+
+
+//Add a scroll to the top button
+
+toTop.addEventListener('click', function(){
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+    })
+})
+
+
+//performance Code to console
+const t1 = performance.now();
+console.log(t1 - t0);
